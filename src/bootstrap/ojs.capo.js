b8ec592bb8fade3bb8a1c53e1c0a5577513c0fb2
Peter Lobsinger <plobsing@gmail.com>
2010-12-13 19:11:08 -0500

    generate both Ωη and JS files from the same source. very DRY.

---
+++ src/bootstrap/ojs.capo.js
@@ -0,0 +1,23 @@
+JumpTable = function(choiceOp, choice) {
+  this.choiceOp = choiceOp
+  this.choices = {}
+  this.add(choice)
+}
+JumpTable.prototype.add = function(choice) {
+  var c = choice[0], t = choice[1]
+  if (this.choices[c]) {
+    if (this.choices[c][0] == this.choiceOp)
+      this.choices[c].push(t)
+    else
+      this.choices[c] = [this.choiceOp, this.choices[c], t]
+  }
+  else
+    this.choices[c] = t
+}
+JumpTable.prototype.toTree = function() {
+  var r = ['JumpTable'], choiceKeys = ownPropertyNames(this.choices)
+  for (var i = 0; i < choiceKeys.length; i += 1)
+    r.push([choiceKeys[i], this.choices[choiceKeys[i]]])
+  return r
+}
+
---
+++ src/bootstrap/ojs.coda.js
@@ -0,0 +1,23 @@
+/* winxed-compiler */
+WinxedParser.hexDigits = "0123456789abcdef"
+WinxedParser.keywords  = { }
+keywords = [
+  "break", "case", "catch", "continue", "default", "do", "else", "for", "function", "if", "in",
+  "instanceof", "new", "return", "switch", "this", "throw", "try", "var", "while", "ometa" ]
+for (var idx = 0; idx < keywords.length; idx++)
+  WinxedParser.keywords[keywords[idx]] = true
+WinxedParser._isKeyword = function(k) { return this.keywords.hasOwnProperty(k) }
+
+/* ometa-compiler */
+BSOMetaTranslator.jumpTableCode = function(cases) {
+  var buf = new StringBuffer()
+  buf.nextPutAll("(function(){switch(this._apply('anything')){")
+  for (var i = 0; i < cases.length; i += 1)
+    buf.nextPutAll("case " + cases[i][0] + ":return " + cases[i][1] + ";")
+  buf.nextPutAll("default: throw fail}}).call(this)")
+  return buf.contents()
+}
+
+/* ometa-optimizer */
+BSNullOptimization.initialize = function() { this._didSomething = false }
+
